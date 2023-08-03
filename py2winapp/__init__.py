e6235fbe7bda53c8a5c7a32062bd7e9a81b02ac8
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2023-08-03 20:03:03 +0300

    docs: add docstrings

--- py2winapp/__init__.py
+++ py2winapp/__init__.py
@@ -1,6 +1,20 @@
+"""Convert a Python script into a Windows application.
+
+Usage:
+    To use this module, simply import the `build` function from the `py2winapp` module
+    and call it with the appropriate arguments.
+
+Example:
+    from py2winapp import build
+    build(
+        python_version="3.8.5",
+        source_dir="src",
+        requirements_file="requirements_prod.txt"
+    )
+"""
 from .py2winapp import build  # noqa
 
 __app_name__ = "py2winapp"
 __version__ = "0.1.0"
-__description__ = "Python to Windows Application"
+__description__ = "Make runnable apps from your python scripts!"
 __author__ = "Ruslan Iskov"
--- py2winapp/downloader.py
+++ py2winapp/downloader.py
@@ -1,3 +1,5 @@
+"""Ddownloader class for downloading files from a given URL to a local file path."""
+
 from pathlib import Path
 
 import requests
@@ -5,12 +7,34 @@ from loguru import logger
 
 
 class Dwwnloader:
+    """Class for downloading files from a given URL to a local file path."""
+
     def __init__(self, download_dir_path: Path):
+        """
+        Initialize the Downloader object.
+
+        Args:
+            download_dir_path (Path): The path to the directory
+            where downloaded files will be saved.
+        """
         if not download_dir_path.exists() or not download_dir_path.is_dir():
             raise FileNotFoundError(f"{download_dir_path} is not found.")
         self._download_dir_path = download_dir_path
 
     def download(self, url: str, file: str) -> Path:
+        """
+        Download a file from a given URL and save it to a local file path.
+
+        It will check if the file is already downloaded and cached. If so, it will
+        return the cached file path. Otherwise, it will download the file and save it
+
+        Args:
+            url (str): The URL of the file to download.
+            file (str): The name of the file to save the downloaded file to.
+
+        Returns:
+            Path: The local file path of the downloaded file.
+        """
         logger.debug(f"Downloading {url} to {file}")
         file_path = self._download_dir_path / file
         if file_path.exists() and file_path.is_file():  # cached
